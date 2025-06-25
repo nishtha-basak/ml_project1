edcc31b83e5b5529996ed4472ef66f5d522e74ba
NB <nishthabasak2003@gmail.com>
2025-06-25 16:47:31 +0530

    logging and exception

--- src/exception.py
+++ src/exception.py
@@ -1,8 +1,10 @@
-import sys
+import sys 
 from src.logger import logging
 
 def error_message_detail(error,error_detail:sys):
     _,_,exc_tb=error_detail.exc_info()
+    #exc_tb is a traceback object that contains information about the exception
+    #exc_tb.tb_frame is the frame object where the exception occurred
     file_name=exc_tb.tb_frame.f_code.co_filename
     error_message="Error occured in python script name [{0}] line number [{1}] error message[{2}]".format(
      file_name,exc_tb.tb_lineno,str(error))
